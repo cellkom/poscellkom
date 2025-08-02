b42567571e3c2cecc0f1a3176caeb17ffa7dbf79
[dyad] <ckcellkom@gmail.com>
2025-08-02 09:44:31 +0700

    [dyad] Memperbaiki kesalahan sintaks pada halaman produk dan menyarankan rebuild - wrote 1 file(s)

--- src/pages/ProductsPage.tsx
+++ src/pages/ProductsPage.tsx
@@ -89,7 +89,6 @@ const ProductsPage = () => {
                   <Skeleton className="h-5 w-3/4" />
                   <Skeleton className="h-4 w-1/2" />
                   <Skeleton className="h-6 w-1/3 mt-2" />
-                  <Skeleton className="h-10 w-full mt-4" />
                 </CardContent>
               </Card>
             ))
