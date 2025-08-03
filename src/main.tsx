5caa4d259e60f9fab195b14ce782eeaeee28798d
[dyad] <ckcellkom@gmail.com>
2025-08-03 07:02:51 +0700

    [dyad] Memindahkan AuthProvider ke dalam Router - wrote 2 file(s)

--- src/main.tsx
+++ src/main.tsx
@@ -1,13 +1,10 @@
 import { createRoot } from "react-dom/client";
 import App from "./App.tsx";
 import "./globals.css";
-import { AuthProvider } from "./contexts/AuthContext.tsx"; // Import AuthProvider
 import React from "react"; // Import React for StrictMode
 
 createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
-    <AuthProvider>
-      <App />
-    </AuthProvider>
+    <App />
   </React.StrictMode>
 );
